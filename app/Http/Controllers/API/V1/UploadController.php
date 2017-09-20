728b54bc0e7041deeb565795f391da7372f04bfb
DevZeng <devzeng@sennki.com>
2017-09-20 14:24:44 +0800

    add Upload

--- app/Http/Controllers/API/V1/UploadController.php
+++ app/Http/Controllers/API/V1/UploadController.php
@@ -8,4 +8,40 @@ use App\Http\Controllers\Controller;
 class UploadController extends Controller
 {
     //
+    public function uploadImage(Request $request)
+    {
+        $file = $request->file('image');
+        $name = $file->getClientOriginalName();
+        $name = explode('.',$name);
+        if (count($name)!=2){
+            return response()->json([
+                'code'=>'ERROR',
+                'msg'=>'非法文件名'
+            ]);
+        }
+        $allow = \Config::get('fileAllow');
+        if (!in_array($name[1],$allow)){
+            return response()->json([
+                'code'=>'ERROR',
+                'msg'=>'不支持的文件格式'
+            ]);
+        }
+        $md5 = md5_file($file);
+        $name = $name[1];
+        $name = $md5.'.'.$name;
+        if (!$file){
+            return response()->json([
+                'code'=>'ERROR',
+                'msg'=>'空文件'
+            ]);
+        }
+        if ($file->isValid()){
+            $destinationPath = 'uploads';
+            $file->move($destinationPath,$name);
+            return response()->json([
+                'code'=>'OK',
+                'base_url'=>$destinationPath.'/'.$name
+            ]);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('test','API\V1\UserController@test');
+Route::group(['prefix'=>'v1'],function (){
+    Route::post('login','API\V1\UserController@OAuthLogin');
+    Route::post('teacher/login','API\V1\UserController@TeacherLogin');
+    Route::post('upload','API\V1\UploadController@uploadImage');
+    Route::post('picture','API\V1\PictureController@addPicture');
+    Route::post('picture/{id}/mark','API\V1\PictureController@addMark');
+});
