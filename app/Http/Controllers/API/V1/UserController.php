e620cc98b1ffd40b598038f0acec791fadb172d0
DevZeng <devzeng@sennki.com>
2017-09-21 16:12:26 +0800

    add teacher

--- app/Http/Controllers/API/V1/UserController.php
+++ app/Http/Controllers/API/V1/UserController.php
@@ -118,7 +118,7 @@ class UserController extends Controller
         $date = date('Y-m-01 0:0:0',strtotime($time));
         $end = date('Y-m-d 23:59:59', strtotime("$date +1 month -1 day"));
         $id = getTeacherToken(Input::get('token'));
-        $count = Picture::where('state','=',2)->where('teacher_id','=',$id)->whereBetween('created_at', ["'".$date."'","'".$end."'" ])->sum('price')->first();
+        $count = Picture::where('state','=',2)->where('teacher_id','=',$id)->whereBetween('created_at', ["'".$date."'","'".$end."'" ])->sum('price');
         return response()->json([
             'code'=>'OK',
             'data'=>$count
