d9481161386a2ecb107ad05c74111fb3d700a324
DevZeng <devzeng@sennki.com>
2017-09-25 16:01:12 +0800

    modify wechat config

--- config/wxxcx.php
+++ config/wxxcx.php
@@ -7,8 +7,11 @@
  */
 return [
     'app_id'=>'wxd5b7e9a51e7e9dde',
-    'secret'=>'fc6c2b662f6131f1a077098826c6256b',
-    'api_key'=>'fc6c2b662f6131f1a088098826c6256b',
-    'mch_id'=>'1489356262',
+    'secret'=>'2d0f3654e43976f10ffc706385dc7185',
+//    'secret'=>'fc6c2b662f6131f1a077098826c6256b',
+    'api_key'=>'Sennki2017SYSg030571XRkh7788XsHx',
+//    'api_key'=>'fc6c2b662f6131f1a088098826c6256b',
+    'mch_id'=>'1487029332',
+//    'mch_id'=>'1489356262',
     'notify_url'=>'http://119.23.255.177:8089/api/v1/pay/notify'
 ];
