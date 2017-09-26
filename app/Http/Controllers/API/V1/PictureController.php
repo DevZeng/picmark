5cf3b4d7e0c4e90af7881b53bf785e61d6bb65e5
DevZeng <devzeng@sennki.com>
2017-09-26 09:50:13 +0800

    fix get picture

--- app/Http/Controllers/API/V1/PictureController.php
+++ app/Http/Controllers/API/V1/PictureController.php
@@ -108,7 +108,8 @@ class PictureController extends Controller
                 'message'=>"没找到该图片！"
             ],404);
         }
-        $picture->mark = $picture->mark();
+        $picture->mark = $picture->mark()->get();
+        $picture->teacher = Teacher::find($picture->teacher_id)->name;
         return response()->json([
             'code'=>"OK",
             'data'=>$picture
